03095f10ff5989ae167cd572c1846c143c3913fc
Monish049 <100188068+Monish049@users.noreply.github.com>
2022-03-01 17:35:25 +0530

    book

---
+++ john/src/App.js
@@ -0,0 +1,13 @@
+import BookList from './components/book_p';
+import './App.css';
+
+
+const App=()=>{
+  return (
+    <div className="App">
+    <BookList/>
+    </div>
+  );
+}
+
+export default App;
